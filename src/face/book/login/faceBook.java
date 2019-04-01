874d18fd90c41866eae41e9a1e75e57abd4eacc1
VKalaivani <kalaivanivelusamy2302@gmail.com>
2019-04-01 13:01:14 +0530

    Sub method added

--- src/face/book/login/faceBook.java
+++ src/face/book/login/faceBook.java
@@ -2,6 +2,10 @@ package face.book.login;
 
 public class faceBook {
 	public void add() {
-	System.out.println("add");
-}
+		System.out.println("add");
+	}
+
+	public void sub() {
+		System.out.println("sub");
+	}
 }
